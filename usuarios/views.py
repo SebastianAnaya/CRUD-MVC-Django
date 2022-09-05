cf906ccd90048da8f9b887c79c39325b3c94de80
SebastianAnaya <sebastianyolo7@hotmail.com>
2022-09-05 17:44:18 -0500

    actualizacion

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,19 @@
+import http
+from http.client import HTTPResponse
+from multiprocessing import context
+from re import template
+from unittest import loader
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from mvcCRUD.usuarios.models import persona
 
 # Create your views here.
+def index(request):
+    usuarios =  persona.objects.all()
+    template = loader.get_template('usuarios/index.html')
+    context={
+        'usuarios': usuarios,
+    }
+    return HttpResponse(template.render(context, request))  
